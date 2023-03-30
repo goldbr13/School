c0ee154616bbb85db3fb5a8a0be82c8c0c6d402f
goldbr13 <goldbr13@tcnj.edu>
2023-03-30 12:36:44 -0400

    first commit

---
+++ Assets/movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Animator anim;
+    private bool isAnimationFinished = false;
+    private Vector3 translation = new Vector3(0, 0, 5);
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("idle", false);
+        Invoke("NotWalking", 39);
+    }
+
+    void Walking()
+    {
+        // Set the flag to allow movement to proceed
+        isAnimationFinished = true;
+    }
+
+    void NotWalking(){
+        isAnimationFinished = false;
+        anim.SetBool("idle", true);
+        transform.Rotate(0, 180, 0);
+    }
+
+    void Update()
+    {
+        // Move the object after the animation has finished
+        if (isAnimationFinished)
+        {
+            transform.Translate(translation * Time.deltaTime);
+        }
+    }
+}
